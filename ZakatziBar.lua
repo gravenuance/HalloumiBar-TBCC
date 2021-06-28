9a051eccc83aebd5b10a6290aa6f49e640d88472
Markos <64923455+gravenuance@users.noreply.github.com>
2021-06-28 23:11:33 +0200

    Update ZakatziBar.lua

--- ZakatziBar.lua
+++ ZakatziBar.lua
@@ -418,7 +418,7 @@ local function zb_commands(sub_string)
 end
 
 local function zb_on_load(self)
-    print("ZakatziBar loaded. Type /zb for more info.")
+    print("|cFFFFB6C1ZakatziBar|r loaded. Type |cFFFFB6C1/zb|r for more info.")
     self:RegisterEvent("COMBAT_LOG_EVENT_UNFILTERED")
     self:RegisterEvent("PLAYER_ENTERING_WORLD")
     zb_initialize_bars()
