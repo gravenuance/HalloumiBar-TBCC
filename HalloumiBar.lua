2f6677223b031c2b0947c608c1b746bfe80b9700
Markos <64923455+gravenuance@users.noreply.github.com>
2021-11-05 20:23:48 +0100

    Update HalloumiBar.lua

--- HalloumiBar.lua
+++ HalloumiBar.lua
@@ -310,7 +310,7 @@ local function hb_combat_log(...)
     if (inInstance == (false or nil)) then
         return
     end
-    if (inInstance and instanceType ~= "pvp" and instanceType ~= "pvp" and instanceType ~= "none") then
+    if (inInstance and instanceType ~= "arena" and instanceType ~= "pvp" and instanceType ~= "none") then
         return
     end
     if is_disabled then
